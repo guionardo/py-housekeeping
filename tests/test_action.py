097473079558b08a64165eebfeb015e2c6be19b9
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2021-04-22 21:59:21 -0300

    Testing

--- tests/test_action.py
+++ tests/test_action.py
@@ -9,3 +9,10 @@ class TestAction(unittest.TestCase):
     def test_invalid_action_config(self):
         with self.assertRaises(ValueError):
             _ = Action({'action': 'error'})
+
+    def test_valid_action_config(self):
+        action = Action({'action': 'delete',
+                        'action_destiny': '.'})
+        self.assertIsInstance(str(action), str)
+        action = Action({'action': 'move',
+                        'action_destiny': '.'})
